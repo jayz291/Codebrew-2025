caf66441afde68558bb0783bef98b57a59d49a8b
Junny20 <liujohn4467@gmail.com>
2025-04-25 16:45:51 +1000

    New

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 french_words = pd.read_csv("french_words.csv")
 french_to_english_dict = {row.French: row.English for (index,row) in french_words.iterrows()}
 print(french_to_english_dict)
+image = Image.open("/images/card_front.png")
 
 #st.write("Hello World!")
-st.file_uploader()
+st.image(image, caption="Starting")
