9d399d3fa3cc2ca030f026dada32f6f7dd9ae7be
Junny20 <liujohn4467@gmail.com>
2025-04-25 18:38:54 +1000

    New Functionality

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
 import streamlit as st
 import pandas as pd
-import time
 from random import randint
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 # Load data
 french_words = pd.read_csv("french_words.csv")
 french_to_english_dict = {row.French: row.English for (index,row) in french_words.iterrows()}
-image = Image.open("images/card_front.png")
 
-# Header
+# Select a random French word
+random_french_word = list(french_to_english_dict.keys())[randint(0, len(french_to_english_dict))]
+english_translation = french_to_english_dict[random_french_word]
+
+# Using PILLOW to inscribe text onto the image
+image = Image.open("images/card_front.png")
+draw = ImageDraw.Draw(image)
+arial_font = ImageFont.truetype("arial.ttf", size=100)
+
+# Get the size of the image and the text
+(image_width, image_height) = image.size
+box_of_text_in_pixels = draw.textbbox(xy=(0, 0), text=random_french_word, font=arial_font)
+text_width = box_of_text_in_pixels[2] - box_of_text_in_pixels[0]
+text_height = box_of_text_in_pixels[3] - box_of_text_in_pixels[1]
+
+# darwing text
+draw.text(xy=((image_width - text_width)/2,(image_height - text_height)/2), text=random_french_word, align="left", fill="black", font=arial_font)
+
 st.markdown("""
     <div style='background-color: DodgerBlue; text-align: center; padding: 10px;'>
         <h1 style='color: Tomato; margin: 0;'>Test your French!</h1>
     </div>""", unsafe_allow_html=True)
 
+image_displayed = st.image(image, caption="French Flashcards", use_container_width=True)
+
 # Create layout with columns
 left, middle, right = st.columns(3)
 
-# Add buttons for user interaction
 left_button = left.button("I don't know this word... :sunglasses:", type="primary")
 right_button = right.button('I know this word!', type="primary")
 
-# Add image
-middle.image(image, caption="French Flashcards", use_container_width=True)
+if left_button:
+    # Draw English translation
+    draw.text(
+        xy=((image_width - text_width) / 2, (image_height - text_height) / 2 + 120),
+        text=french_to_english_dict[random_french_word],
+        fill="blue",
+        font=arial_font
+    )
+    image_displayed = st.image(image, caption="Translation shown!", use_column_width=True)
 
-for i in range(len(french_to_english_dict)):
-    random_french_word = list(french_to_english_dict.keys())[randint(0, len(french_to_english_dict))]
-    middle.write(random_french_word)
-    time.sleep(1)
+elif right_button:
+    st.success("Nice job!")
+
+
+
+
 
 
 
